2c9739304d854330c3991a0c12f547d1a5558877
Melllzzzz <pacamo.rammel29@gmail.com>
2026-04-25 20:34:53 +0800

    Functional Login

--- api/signals.py
+++ api/signals.py
@@ -4,13 +4,12 @@ from .models import User, TeacherProfile, StudentProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    # This runs the exact millisecond a new User is inserted into the database
     if created:
         if instance.status == 'TEACHER':
             TeacherProfile.objects.create(
                 user=instance,
-                name=instance.full_name,
-                specialty="General",
+                display_name=instance.full_name, # Changed from 'name'
+                department="General",            # Changed from 'specialty'
                 organization="VocaLink"
             )
         elif instance.status == 'STUDENT':
